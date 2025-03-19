4194ac33594dddbb33a5c7fb8c6e1906dd4271ec
Syaha Creator <msyahrul090@gmail.com>
2025-03-20 00:34:27 +0700

    Update configuration app

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/network_provider.dart';
+import 'routes/app_routes.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(themeMode: ThemeMode.system);
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => NetworkProvider())],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.system,
+        routerConfig: AppRoutes.router,
+      ),
+    );
   }
 }
